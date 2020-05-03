d1d2b76b9fc5db5459ba121444fdaeccd51116a4
KotBegemot56 <khotsenkos1993k@gmail.com>
2020-05-03 15:50:51 +0300

    Weather parser bot

--- chat_bot.py
+++ chat_bot.py
@@ -19,7 +19,7 @@ def welcome(message):
     bot.send_message(message.chat.id,
                      "Hello, {0.first_name}!\n"
                      "I am - <b>{1.first_name}</b>.\n"
-                     "Please write the name of the city whose weather you want to know".format(
+                     "Please, write the name of the city for witch you need to look up the weather".format(
                          message.from_user, bot.get_me()),
                      parse_mode='html', reply_markup=markup)
 
@@ -32,53 +32,10 @@ def conversation(message):
         if r.status_code == 200:
             for el in html.select('#content'):
                 temperature = el.select('.today-temp')[0].text
-                bot.send_message(message.chat.id, 'Today in' + ' ' + message.text.title() + ' ' + temperature)
+                bot.send_message(message.chat.id, 'Now in ' + message.text.title() + ' ' + temperature)
         else:
-            bot.send_message(message.chat.id, 'Cannot find that town, please try another town')
-
-
-# @bot.message_handler(content_types=['text'])
-# def conversation(message):
-#     if message.chat.type == 'private':
-#         if message.text == 'Weather':
-#             # info about weather
-#             bot.send_message(message.chat.id, 'Today in Kyiv' + ' ' + temperature)
-#         elif message.text == 'Random number':
-#             bot.send_message(message.chat.id, str(random.randint(0, 100)))
-#         elif message.text == 'Photo':
-#             sti = open('static/corgey2.jpeg', 'rb')
-#             bot.send_photo(message.chat.id, sti)
-#         elif message.text == 'How are you?':
-#             markup = types.InlineKeyboardMarkup(row_width=2)
-#             item1 = types.InlineKeyboardButton("Good", callback_data='good')
-#             item2 = types.InlineKeyboardButton("Bad", callback_data='bad')
-#
-#             markup.add(item1, item2)
-#
-#             bot.send_message(message.chat.id, 'Fine, and yours?', reply_markup=markup)
-#         else:
-#             bot.send_message(message.chat.id, 'I don`t know what to answer.')
-
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    try:
-        if call.message:
-            if call.data == 'good':
-                bot.send_message(call.message.chat.id, 'It`s fine')
-            elif call.data == 'bad':
-                bot.send_message(call.message.chat.id, 'Hold on')
-
-            # remove inline buttons
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="How are you?",
-                                  reply_markup=None)
-
-            # show alert
-            bot.answer_callback_query(callback_query_id=call.id, show_alert=True,
-                                      text="Something is written")
-
-    except Exception as e:
-        print(repr(e))
+            bot.send_message(message.chat.id,
+                             'Cannot find that town ' + message.text.title() + '\nPlease try another city')
 
 
 # RUN
