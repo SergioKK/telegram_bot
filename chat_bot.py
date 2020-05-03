c1552cef9dae9ecf23e8e154f6c5b790b62d914b
KotBegemot56 <khotsenkos1993k@gmail.com>
2020-05-03 11:11:18 +0300

    Weather parser bot

--- chat_bot.py
+++ chat_bot.py
@@ -1,8 +1,8 @@
 import telebot
 import config
-import random
-from weather_forecast_bot import temperature
 from telebot import types
+import requests
+from bs4 import BeautifulSoup
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -11,16 +11,15 @@ bot = telebot.TeleBot(config.TOKEN)
 def welcome(message):
     # keyboard
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Random number")
-    item2 = types.KeyboardButton("How are you?")
+    item1 = types.KeyboardButton("Киев")
+    item2 = types.KeyboardButton("Одесса")
 
     markup.add(item1, item2)
 
     bot.send_message(message.chat.id,
-                     "Hello!, {0.first_name}!\n"
+                     "Hello, {0.first_name}!\n"
                      "I am - <b>{1.first_name}</b>.\n"
-                     "Type Weather to get info about weather today in Kyiv\n"
-                     "Type Photo and i will send you an interesting photo".format(
+                     "Please write the name of the city whose weather you want to know".format(
                          message.from_user, bot.get_me()),
                      parse_mode='html', reply_markup=markup)
 
@@ -28,24 +27,37 @@ def welcome(message):
 @bot.message_handler(content_types=['text'])
 def conversation(message):
     if message.chat.type == 'private':
-        if message.text == 'Weather':
-            # info about weather
-            bot.send_message(message.chat.id, 'Today in Kyiv' + ' ' + temperature)
-        elif message.text == 'Random number':
-            bot.send_message(message.chat.id, str(random.randint(0, 100)))
-        elif message.text == 'Photo':
-            sti = open('static/corgey2.jpeg', 'rb')
-            bot.send_photo(message.chat.id, sti)
-        elif message.text == 'How are you?':
-            markup = types.InlineKeyboardMarkup(row_width=2)
-            item1 = types.InlineKeyboardButton("Good", callback_data='good')
-            item2 = types.InlineKeyboardButton("Bad", callback_data='bad')
-
-            markup.add(item1, item2)
-
-            bot.send_message(message.chat.id, 'Fine, and yours?', reply_markup=markup)
+        r = requests.get('https://sinoptik.ua/погода-' + message.text)
+        html = BeautifulSoup(r.content, 'html.parser')
+        if r.status_code == 200:
+            for el in html.select('#content'):
+                temperature = el.select('.today-temp')[0].text
+                bot.send_message(message.chat.id, 'Today in' + ' ' + message.text.title() + ' ' + temperature)
         else:
-            bot.send_message(message.chat.id, 'I don`t know what to answer.')
+            bot.send_message(message.chat.id, 'Cannot find that town, please try another town')
+
+
+# @bot.message_handler(content_types=['text'])
+# def conversation(message):
+#     if message.chat.type == 'private':
+#         if message.text == 'Weather':
+#             # info about weather
+#             bot.send_message(message.chat.id, 'Today in Kyiv' + ' ' + temperature)
+#         elif message.text == 'Random number':
+#             bot.send_message(message.chat.id, str(random.randint(0, 100)))
+#         elif message.text == 'Photo':
+#             sti = open('static/corgey2.jpeg', 'rb')
+#             bot.send_photo(message.chat.id, sti)
+#         elif message.text == 'How are you?':
+#             markup = types.InlineKeyboardMarkup(row_width=2)
+#             item1 = types.InlineKeyboardButton("Good", callback_data='good')
+#             item2 = types.InlineKeyboardButton("Bad", callback_data='bad')
+#
+#             markup.add(item1, item2)
+#
+#             bot.send_message(message.chat.id, 'Fine, and yours?', reply_markup=markup)
+#         else:
+#             bot.send_message(message.chat.id, 'I don`t know what to answer.')
 
 
 @bot.callback_query_handler(func=lambda call: True)
